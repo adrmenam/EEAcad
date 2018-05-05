f153b9ba91560c0af95fe01e33946b58fef2ce85
Inti Guzman <intiguzmanc@gmail.com>
2018-05-05 10:53:12 -0500

    F_Login_02
    Delete strange word

--- eeuio/login.php
+++ eeuio/login.php
@@ -68,7 +68,7 @@
   								        }
                         }
                 }else{
-                  echo "hkdsbacjasd";
+                  echo "";
                 }
 							 ?>
 						</div>
