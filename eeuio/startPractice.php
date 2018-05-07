7123c71133f2f0d2153ddcb643a4b3afd36fa65d
adrmenam <adrian@kayinnovation.com>
2018-05-06 19:08:49 -0500

    F_StartPracticeButton_02
    Create PracticeCanvas
    Prepare startPractice.php

--- eeuio/startPractice.php
+++ eeuio/startPractice.php
@@ -1,13 +1,22 @@
 <?php
 include 'dbconnection.php';
+include 'redirect.php';
 
 if($_POST){
-  $username=$_SESSION["username"];
-  print($username);
+  $username=$_POST["username"];
+  //print($username);
   $type=$_POST["type"];
-  print($type);
 
+  $sql = "INSERT INTO USUARIO (USU_PASSWORD, USU_EMAIL, USU_CEDULA, USU_ROL, USU_NOMBRES, USU_APELLIDOS)
+  VALUES (MD5('".$password."'), '".$emailaddress."', '".$ci."', 'Estudiante', '".$firstname."', '".$lastname."' )";
+
+  if ($mysqli->query($sql) === TRUE) {
+      redirect('practiceCanvas.php');
+  } else {
+      echo "Error: " . $sql . "<br>" . $mysqli->error;
+  }
+
+  $mysqli->close();
 }
-echo("hola");
 
 ?>
