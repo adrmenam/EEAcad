18884be8d1d94f79cf1d096b6e270a4d7c0d3bd7
Inti Guzman <intiguzmanc@gmail.com>
2018-06-26 22:16:49 -0500

    F_DBConnection_02
    ver errores

--- eeuio/dbconnection.php
+++ eeuio/dbconnection.php
@@ -16,7 +16,7 @@
 
   $mysqli = new mysqli($server, $user, $password, $db);
   $mysqli->set_charset("utf8");
-  if (!$enlace) {
+  if (!$mysqli) {
     echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
     echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
     echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
